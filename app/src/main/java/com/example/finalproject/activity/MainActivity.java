ea6e584bcf397ae60299c9cd64fe6f031b798cee
hanaa <hanaahany822@gmail.com>
2021-01-03 19:01:58 +0200

    NavigationDrawerLayout

--- app/src/main/java/com/example/finalproject/activity/MainActivity.java
+++ app/src/main/java/com/example/finalproject/activity/MainActivity.java
@@ -1,16 +1,84 @@
 package com.example.finalproject.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.finalproject.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    NavController navController;
+    AppBarConfiguration appBarConfiguration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        initViews();
+
+         appBarConfiguration =
+                new AppBarConfiguration.Builder(R.id.homeFragment,R.id.participantFragment)
+                        .setDrawerLayout(drawerLayout)
+                        .build();
+        NavigationUI.setupActionBarWithNavController(MainActivity.this,navController,appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_end,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id=item.getItemId();
+        if(id==R.id.end){
+            Toast.makeText(this, "ENd", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initViews() {
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.nav_view);
+        navController= Navigation.findNavController(MainActivity.this,R.id.nav_host_fragment);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController,appBarConfiguration);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        else
+
+        super.onBackPressed();
+    }
+    }
